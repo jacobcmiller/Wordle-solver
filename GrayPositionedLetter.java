7ad88ce5d5cb5ee64400f27f9eacee3af9c1b64b
jacobcmiller <jacobcmiller1723@gmail.com>
2025-01-31 16:21:31 -0500

    GrayPositionedLetter.java

---
+++ GrayPositionedLetter.java
@@ -0,0 +1,12 @@
+package model;
+// Class for gray letters with a specific position
+public class GrayPositionedLetter {
+    public char letter;
+    public int position;
+
+    // Constructor for initializing a gray letter and its position
+    public GrayPositionedLetter(char letter, int position) {
+        this.letter = letter;
+        this.position = position;
+    }
+}

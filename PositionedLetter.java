59acf9f9e3b14058d582371f67fd6fe901c52f68
jacobcmiller <jacobcmiller1723@gmail.com>
2025-01-31 16:23:53 -0500

    PositionedLetter.java

---
+++ PositionedLetter.java
@@ -0,0 +1,12 @@
+package model;
+// Class for letters (yellow and green) with a specific position
+public class PositionedLetter {
+    public char letter;
+    public int position;
+
+    // Constructor for initializing a letter and its position
+    public PositionedLetter(char letter, int position) {
+        this.letter = letter;
+        this.position = position;
+    }
+}
